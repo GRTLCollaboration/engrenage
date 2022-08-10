update odeint control, fix some scaling

myinitialconditions.py
@@ -12,7 +12,7 @@ from scipy.interpolate import interp1d
 def get_initial_vars_values() :
 
     initial_vars_values = np.zeros(NUM_VARS * N)
-
+    
     # Use oscilloton data to construct functions for the vars
     grr0_data    = np.loadtxt("source/initial_data/grr0.csv")
     lapse0_data  = np.loadtxt("source/initial_data/lapse0.csv")

myparams.py
@@ -5,10 +5,10 @@
 import numpy as np
 
 # Input parameters for grid and evolution here
-N_r = 120 # num points on physical grid
+N_r = 240 # num points on physical grid
 N_t = 101 # time resolution (only for outputs, not for integration)
-R = 60 # Maximum outer radius
-T = 3.0 # Maximum evolution time
+R = 120 # Maximum outer radius
+T = 2.0 # Maximum evolution time
 
 # coefficients for bssn and gauge evolution
 eta = 1.0 # 1+log slicing damping coefficient
@@ -20,6 +20,10 @@ scalar_mu = 1.0 # this is an inverse length scale related to the scalar compton 
 # so should not be changed
 dx = R/N_r
 dt = T/N_t
+# for control of odeint
+max_dt = 0.1 * dx # Enforce courant condition
+min_dt = max_dt / 100 # Don't want tiny steps - give up if too small
+max_steps = int(dt / min_dt)
 num_ghosts = 3
 N = N_r + num_ghosts * 2 
 r = np.linspace(-(num_ghosts-0.5)*dx, R+(num_ghosts-0.5)*dx, N)

source/tensoralgebra.py
@@ -119,26 +119,20 @@ def get_A_LL(r_here, a):
     return np.diag( scaling * scaling * np.diag(a) )
 
 # Compute the \bar A^ij given A_ij and \bar\gamma^ij
-def get_A_UU(A_LL, bar_gamma_UU):
+def get_A_UU(a, r_gamma_UU):
     
     # Note that bar_gamma_UU is symmetric.
     # If it wasn't symmetric, we would need to be careful about
     # transposing when multiplying from the right.
     # multi_dot is kind of overkill here for 3x3 matrices...
     # but it's good to know about!
-    return np.linalg.multi_dot( [ bar_gamma_UU, A_LL, bar_gamma_UU ] )
+    return np.linalg.multi_dot( [ r_gamma_UU, a, r_gamma_UU ] )
 
 # Compute trace of (traceless part of) extrinsic curvature
-def get_trace_A(r_here, a, bar_gamma_UU) :
-
-    scaling = np.array([1.0, r_here , r_here*sintheta]) 
-    
-    trace_A = 0.0
-    for i in range(0, SPACEDIM):
-        for j in range(0, SPACEDIM):    
-            trace_A += scaling[i] * scaling[j] * bar_gamma_UU[i][j] * a[i][j]
+def get_trace_A(r_here, a, r_gamma_UU) : 
        
-    return trace_A
+    # Matrix multiply, then matrix trace
+    return np.trace( np.dot(r_gamma_UU, a ) )
 
 # Compute trace of some rank 2 tensor with indices lowered
 def get_trace(T_LL, gamma_UU):
@@ -147,18 +141,15 @@ def get_trace(T_LL, gamma_UU):
     return np.trace( np.dot(gamma_UU, T_LL ) )
 
 # Compute A_ij A^ij
-def get_Asquared(r_here, a, bar_gamma_UU) :
-
-    scaling = np.array([1.0, r_here , r_here*sintheta])
+def get_Asquared(r_here, a, r_gamma_UU) :
     
     Asquared = 0.0
     for i in range(0, SPACEDIM):
         for j in range(0, SPACEDIM):    
             for k in range(0, SPACEDIM):
                 for l in range(0, SPACEDIM):  
-                    Asquared += (scaling[i] * scaling[j] * a[i][j]
-                                 * scaling[k] * scaling[l] * a[k][l]
-                                 * bar_gamma_UU[i][k] * bar_gamma_UU[j][l])
+                    Asquared += ( a[i][j] *  a[k][l]
+                                 * r_gamma_UU[i][k] * r_gamma_UU[j][l])
        
     return Asquared
 